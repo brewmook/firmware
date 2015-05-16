6f523eb7a122687de1c7caf03157e0a09d53b61d
Dave E <dave@brewmook.com>
2015-05-16 23:01:13 +0100

    Basic temp and pressure sensor readings.

--- src/application.cpp
+++ src/application.cpp
@@ -1,176 +1,70 @@
-/**
- ******************************************************************************
- * @file    application.cpp
- * @authors  Satish Nair, Zachary Crockett and Mohit Bhoite
- * @version V1.0.0
- * @date    05-November-2013
- * @brief   Tinker application
- ******************************************************************************
-  Copyright (c) 2013 Spark Labs, Inc.  All rights reserved.
+#include "OneWire/OneWire.h"
+#include "spark-dallas-temperature/spark-dallas-temperature.h"
+#include <math.h>
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation, either
-  version 3 of the License, or (at your option) any later version.
+OneWire ds(D3);
+DallasTemperature sensors(&ds);
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+double pressure = 0;
+double temperature = 0;
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this program; if not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************
- */
+unsigned long last_executed;
 
-/* Includes ------------------------------------------------------------------*/  
-#include "application.h"
-
-/* Function prototypes -------------------------------------------------------*/
-int tinkerDigitalRead(String pin);
-int tinkerDigitalWrite(String command);
-int tinkerAnalogRead(String pin);
-int tinkerAnalogWrite(String command);
-
-SYSTEM_MODE(AUTOMATIC);
-
-/* This function is called once at start up ----------------------------------*/
-void setup()
-{
-	//Setup the Tinker application here
-
-	//Register all the Tinker functions
-	Spark.function("digitalread", tinkerDigitalRead);
-	Spark.function("digitalwrite", tinkerDigitalWrite);
-
-	Spark.function("analogread", tinkerAnalogRead);
-	Spark.function("analogwrite", tinkerAnalogWrite);
-
+void ledOff() {
+  RGB.color(32,32,32);
 }
 
-/* This function loops forever --------------------------------------------*/
-void loop()
-{
-	//This will run in a loop
+void ledOn(int r, int g, int b) {
+  RGB.color(r, g, b);
 }
 
-/*******************************************************************************
- * Function Name  : tinkerDigitalRead
- * Description    : Reads the digital value of a given pin
- * Input          : Pin 
- * Output         : None.
- * Return         : Value of the pin (0 or 1) in INT type
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerDigitalRead(String pin)
-{
-	//convert ascii to integer
-	int pinNumber = pin.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
+void setup() {
+  setADCSampleTime(ADC_SampleTime_13Cycles5);
+  Serial.begin(9600);  // local hardware test only
+  pinMode(A7, INPUT);
 
-	if(pin.startsWith("D"))
-	{
-		pinMode(pinNumber, INPUT_PULLDOWN);
-		return digitalRead(pinNumber);
-	}
-	else if (pin.startsWith("A"))
-	{
-		pinMode(pinNumber+10, INPUT_PULLDOWN);
-		return digitalRead(pinNumber+10);
-	}
-	return -2;
+  delay(1000);
+  sensors.begin();
+
+  Spark.variable("temperature", &temperature, DOUBLE);
+  Spark.variable("pressure", &pressure, DOUBLE);
+
+  RGB.control(true);
+  RGB.brightness(8);
+  ledOff();
+
+  last_executed = millis();
 }
 
-/*******************************************************************************
- * Function Name  : tinkerDigitalWrite
- * Description    : Sets the specified pin HIGH or LOW
- * Input          : Pin and value
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerDigitalWrite(String command)
+double map(const double & x,
+           const double & in_min, const double & in_max,
+           const double & out_min, const double & out_max)
 {
-	bool value = 0;
-	//convert ascii to integer
-	int pinNumber = command.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
-
-	if(command.substring(3,7) == "HIGH") value = 1;
-	else if(command.substring(3,6) == "LOW") value = 0;
-	else return -2;
-
-	if(command.startsWith("D"))
-	{
-		pinMode(pinNumber, OUTPUT);
-		digitalWrite(pinNumber, value);
-		return 1;
-	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber+10, OUTPUT);
-		digitalWrite(pinNumber+10, value);
-		return 1;
-	}
-	else return -3;
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-/*******************************************************************************
- * Function Name  : tinkerAnalogRead
- * Description    : Reads the analog value of a pin
- * Input          : Pin 
- * Output         : None.
- * Return         : Returns the analog value in INT type (0 to 4095)
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerAnalogRead(String pin)
-{
-	//convert ascii to integer
-	int pinNumber = pin.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
-
-	if(pin.startsWith("D"))
-	{
-		pinMode(pinNumber, INPUT);
-		return analogRead(pinNumber);
-	}
-	else if (pin.startsWith("A"))
-	{
-		pinMode(pinNumber+10, INPUT);
-		return analogRead(pinNumber+10);
-	}
-	return -2;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerAnalogWrite
- * Description    : Writes an analog value (PWM) to the specified pin
- * Input          : Pin and Value (0 to 255)
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerAnalogWrite(String command)
-{
-	//convert ascii to integer
-	int pinNumber = command.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
-
-	String value = command.substring(3);
-
-	if(command.startsWith("D"))
-	{
-		pinMode(pinNumber, OUTPUT);
-		analogWrite(pinNumber, value.toInt());
-		return 1;
-	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber+10, OUTPUT);
-		analogWrite(pinNumber+10, value.toInt());
-		return 1;
-	}
-	else return -2;
+void loop() {
+  unsigned long loop_time = millis();
+  if ((loop_time - last_executed) >= 2000)
+  {
+    ledOn(0,255,0);
+    sensors.requestTemperatures();
+    temperature = sensors.getTempCByIndex(0);
+    pressure = (double)analogRead(A7);
+    Serial.print(pressure, 0);
+    //Serial.print(" raw");
+    Serial.print(", ");
+    Serial.print(map(pressure, 0.0, 4095.0, 0.0, 1.0), 4);
+    //Serial.print(" norm");
+    Serial.print(", ");
+    double v = map(pressure, 0.0, 4095.0, 0.0, 3.3);
+    Serial.print(v, 4);
+    //Serial.print(" V");
+    Serial.print(", ");
+    Serial.print(map(v, 0.6, 3.0, 0.0, 4.0), 4);
+    //Serial.print(" kPa");
+    Serial.print("\n");
+    ledOff();
+    last_executed = loop_time;
+  }
 }
